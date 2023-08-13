59e8727a4f6f5407a0fd73862c38442db6c861a7
qudus4l <mightbeq@gmail.com>
2023-08-13 20:30:18 +0100

    FIX

--- Pytorch/src/train.py
+++ Pytorch/src/train.py
@@ -9,21 +9,28 @@ from data import load_image, device
 class style_transfer_VGG(nn.Module):
     def __init__(self):
         super(style_transfer_VGG, self).__init__()
-
-        # VGG19 layers
+        # The first number x in convx_y gets added by 1 after it has gone
+        # through a maxpool, and the second y if we have several conv layers
+        # in between a max pool. These strings (0, 5, 10, ..) then correspond
+        # to conv1_1, conv2_1, conv3_1, conv4_1, conv5_1 mentioned in NST paper
         self.chosen_features = ["0", "5", "10", "19", "28"]
+
+        # We don't need to run anything further than conv5_1 (the 28th module in vgg)
+        # Since remember, we dont actually care about the output of VGG: the only thing
+        # that is modified is the generated image (i.e, the input).
         self.model = models.vgg19(pretrained=True).features[:29]
 
     def forward(self, x):
         # Store relevant features
         features = []
 
-        # Go through layers
-        for i, layer in enumerate(self.model):
+        # Go through each layer in model, if the layer is in the chosen_features,
+        # store it in features. At the end we'll just return all the activations
+        # for the specific layers we have in chosen_features
+        for layer_num, layer in enumerate(self.model):
             x = layer(x)
 
-            # Store relevant features
-            if str(i) in self.chosen_features:
+            if str(layer_num) in self.chosen_features:
                 features.append(x)
 
         return features
