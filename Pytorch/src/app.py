d4fd7c79344e65f909f2353dc6c141386cdaf798
qudus4l <mightbeq@gmail.com>
2023-08-14 19:09:37 +0100

    fix streamlit

--- Pytorch/src/app.py
+++ Pytorch/src/app.py
@@ -1,33 +1,31 @@
+import numpy as np
 import streamlit as st
-import torch
 from train import train_image
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+st.title("Style Transfer App")
 
-def main():
-    st.title("Style Transfer App")
-    
-    # Upload the content image and style image
-    content_image = st.file_uploader("Upload Content Image", type=["jpg", "png", "jpeg"])
-    style_image = st.file_uploader("Upload Style Image", type=["jpg", "png", "jpeg"])
-    
-    if content_image and style_image:
-        # Save uploaded images to disk
-        content_path = "content_image.jpg"
-        style_path = "style_image.jpg"
-        with open(content_path, "wb") as f:
-            f.write(content_image.read())
-        with open(style_path, "wb") as f:
-            f.write(style_image.read())
-        
-        # Perform style transfer and get generated image
-        generated_image = train_image(content_path, style_path)
-        
-        # Display the generated image in the app
-        st.image(generated_image, caption="Generated Image", use_column_width=True)
+# Upload the content image and style image
+content_image = st.file_uploader("Upload Content Image", type=["jpg", "png", "jpeg"])
+style_image = st.file_uploader("Upload Style Image", type=["jpg", "png", "jpeg"])
 
-if __name__ == "__main__":
-    main()
+if content_image and style_image:
+    # Save uploaded images to disk
+    content_path = "content_image.jpg"
+    style_path = "style_image.jpg"
+    with open(content_path, "wb") as f:
+        f.write(content_image.read())
+    with open(style_path, "wb") as f:
+        f.write(style_image.read())
+    
+    # Perform style transfer and get generated image
+    generated_image = train_image(content_path, style_path)
+    
+    # Convert the tensor to a NumPy array and normalize it
+    generated_np = generated_image.squeeze().cpu().detach().numpy()
+    generated_np = (generated_np - generated_np.min()) / (generated_np.max() - generated_np.min())
+    
+    # Display the generated image in the app
+    st.image(generated_np, caption="Generated Image", use_column_width=True, channels="RGB")
 
 
 
